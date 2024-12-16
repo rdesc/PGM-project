08c71184ad67ee514719bf2509998275b5bef27e
Daniel Lawson <daniellawson9999@gmail.com>
2024-12-16 15:19:41 -0500

    fixed

--- scripts/eval_hf_value.py
+++ scripts/eval_hf_value.py
@@ -124,8 +124,8 @@ if __name__ == "__main__":
         
     pipeline = ValueGuidedRLPipeline(value_function=value_function, unet=diffusion_model, scheduler=scheduler, env=env).to(device)
 
-    config['model_type'] = 'diffuser'
-    config['model_arch'] = class_str
+    config.model_type = 'diffuser'
+    config.model_arch = class_str
     if config.wandb_track:
         wandb.init(
             config=config,
